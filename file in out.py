d7053f09b7082b46b7cc282b41a783fc9fb35749
thanwaimdy <shwethwar28@gmail.com>
2019-03-15 00:11:14 +0630

    adding source codes

---
+++ file in out.py
@@ -0,0 +1,6 @@
+print("Shwethwar Than Wai")
+a = 10
+if a<5:
+    print("This is a book.")
+else:
+    print("GoodBy!!!")
---
+++ wifi hotspot enable.py
@@ -0,0 +1,38 @@
+
+#!/usr/bin/python3
+# wifi hotspot enabler
+
+import os
+os.system('cls')
+print('\n\n\n')
+print('Green Hacker WiFI Hotspot Enaler')
+print('(c) 2019 Green Hackers Group. All right reserved')
+print()
+
+cmd = '0'       # space is very caution between cmd and = , if not ssid and key are miss
+while cmd != '3':
+    print('1-Start Hotspot')
+    print('2-Stop Hotspot')
+    print('3-Exit')
+    cmd = input('Please Enter Your Choice (1,2,3):  ')
+    if cmd == '1':
+        print('Starting Wifi Hotspot....')
+        ssid = input('Please Enter SSID: ')
+        password = input('Please Enter Password: ')
+        command = " netsh wlan set hostednetwork mode = allow ssid= " + ssid + " key= " + password
+        os.system(command)
+        os.system('netsh wlan start hostednetwork')
+    elif cmd == '2':
+        print('Stopping Wifi Hotspot....')
+        os.system('netsh wlan stop hostednetwork')
+    elif cmd == '3':
+        print('Exit Program....')
+        quit()
+    else:
+        print("Bad input! Please try again (Only 1,2,3)")
+        os.system('pause')
+
+# now run this prog but it was run on only administrator mode
+#  os.system("netsh wlan set hostednetwork mode=allow ssid=GreenHackers key=12345678")
+#  os.system('netsh wlan start hostednetwork')
+
