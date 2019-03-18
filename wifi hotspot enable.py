29a9556eb2b173b68e583207bbcf2189cf374766
ThanWai <48476234+thanwaimdy@users.noreply.github.com>
2019-03-18 02:16:55 +0000

    Update wifi hotspot enable.py

--- wifi hotspot enable.py
+++ wifi hotspot enable.py
@@ -1,6 +1,7 @@
 
 #!/usr/bin/python3
 # wifi hotspot enabler
+# AungWinnHtut GHOT
 
 import os
 os.system('cls')
@@ -9,7 +10,7 @@ print('Green Hacker WiFI Hotspot Enabler')
 print('(c) 2019 Green Hackers Group. All right reserved')
 print()
 
-cmd = '0'       # space is very caution between cmd and = , if not ssid and key are miss
+cmd = '0' # space is very caution between cmd and = , if not ssid and key are mixed.
 
 while cmd != '3':
     print('1-Start Hotspot')
@@ -33,9 +34,9 @@ while cmd != '3':
         print("Bad input! Please try again (Only 1,2,3)")
         os.system('pause')
 
-# now run this prog but it was run on only administrator mode
+# now run this code but it was run on only administrator mode.
 #  os.system("netsh wlan set hostednetwork mode=allow ssid=GreenHackers key=12345678")
 #  os.system('netsh wlan start hostednetwork')
 # now all are OK after cmd='0'
-# DONE!  15/3/2019 12:22AM thanks sir AgWinnHtut
+# DONE! 15/3/2019 12:22AM, Thanks Sir AgWinnHtut. 
 
