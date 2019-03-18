52f786e29546384f5e3b660377c8c6e790883117
ThanWai <48476234+thanwaimdy@users.noreply.github.com>
2019-03-18 16:16:21 +0000

    Add files via upload

---
+++ function.py
@@ -0,0 +1,63 @@
+# function  (def)
+# GHOT
+
+def function():
+    
+    "this function print three string line" 
+    # this is not to show ans
+    
+    print("shwe")
+    print("thwar")
+    print("Shwethwar ThanWai")
+    
+# () is same set theory in math.
+# function is the relationship of main and codomain
+# y is function of x   y=f(x)
+
+def fun(name):
+    print("shwe")
+    print("thwar")
+    print("I am", end=" ")
+    print(name)
+    
+def funy(x):
+    y=2*x    # y is function of x
+    print('x = ',y )
+    
+
+def funadd(x , y):  # input two
+    total = x+y
+    return total
+    
+def funadd3(x ,y ,z):  # input three
+    total = x+y+z
+    return total
+    
+function()
+
+fun('mg mg')
+fun('ag ag') 
+
+funy(456)
+funy(123)
+funy(222)
+
+t=funadd(10 , 20)
+print(t)
+
+tot=funadd3(20 ,10 ,30)
+print(tot)
+    
+    
+    
+ 
+def f():
+    """return 'Hello world!'"""
+    return "hello world"
+
+
+print (f())
+print (f. __doc__)  # ans is none what answer
+
+
+
